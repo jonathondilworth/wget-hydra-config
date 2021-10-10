163ad27bc1dee51e0eb174f37b3b92dbf30b57ef
Jonathon Dilworth <jon@dilworth.dev>
2021-10-10 12:43:01 +0100

    sh wget script, quick & hacky

---
+++ fetch.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+wget https://hydra.iohk.io/build/7370192/download/1/mainnet-config.json \
+wget https://hydra.iohk.io/build/7370192/download/1/mainnet-byron-genesis.json \
+wget https://hydra.iohk.io/build/7370192/download/1/mainnet-shelley-genesis.json \
+wget https://hydra.iohk.io/build/7370192/download/1/mainnet-alonzo-genesis.json \
+wget https://hydra.iohk.io/build/7370192/download/1/mainnet-topology.json
